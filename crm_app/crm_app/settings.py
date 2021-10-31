4dcd138a7cca8f8ae0df55a16515bdb8fbdee8fb
MojeCvicneProgramy <renatacarna@gmail.com>
2021-10-31 23:11:26 +0100

    tatic files css a images, vloženie do templatov, nastavenie settings.py

--- crm_app/crm_app/settings.py
+++ crm_app/crm_app/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from os import path
+import os
+import pathlib
+
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR , 'static'),
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
