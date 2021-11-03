c3ce0eebf029a9e40a7a74319ceb8be037ac5980
MojeCvicneProgramy <renatacarna@gmail.com>
2021-11-03 09:31:47 +0100

    Database model queries, Rendering data to templates. Shell pre prácu s dátami. Prepojenie s databázou vo views.py. Vloženie dát do template súborov pomocou Dajango template tags

--- crm_app/crm_app_accounts/models.py
+++ crm_app/crm_app_accounts/models.py
@@ -50,5 +50,7 @@ class Order (models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)  # výber choices ťahá z premennej STATUS zadefinovanej vyššie
 
-    def __str__(self):
-        return self.name
+    # zobrazovanie mena pre Orders nezadávame, potom nefunguje pridávanie
+    # objednávok a vypíše chybu
+    # def __str__(self):
+    #     return self.name
--- crm_app/crm_app_accounts/views.py
+++ crm_app/crm_app_accounts/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *  # naimportovanie celej databázy vytvorenej v models.py
 
-def home(request):
-    return render(request, 'accounts/dashboard.html')
+def home(request):  # dashboard
+    orders = Order.objects.all()  # z databázy zo všetkých objektov v Orders, zobrať všetky a dať do premennej orders
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders':orders, 'customers':customers,
+                'total_orders':total_orders, 'delivered':delivered, 'pending':pending
+    }  # dictionary s ľubovoľným množstvom prvkov
+
+    return render(request, 'accounts/dashboard.html', context)  # tu vytvorené prvky z premennej context použijeme v templates/products.py a templates/status.py
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()  # z databázy Produktov vybrať všetko a uložiť do premennej products
+
+    return render(request, 'accounts/products.html', {'products': products})  # tu vytvorené 'products' použijeme v templates/products.py
 
 def customers(request):
-    return render(request, 'accounts/customer.html')    
- 
+    return render(request, 'accounts/customer.html')
+
+  
